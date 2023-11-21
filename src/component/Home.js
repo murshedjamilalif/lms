95c6341ed3ca147719e13814e412408c299ee49a
Sadman-26 <91084931+Sadman-26@users.noreply.github.com>
2023-11-21 16:57:25 +0600

    Add files via upload

---
+++ src/component/Home.js
@@ -0,0 +1,243 @@
+import {Link} from 'react-router-dom';
+import AllCourses from './AllCourses';
+import PopularCourses from './PopularCourses';
+import PopularTeachers from './PopularTeachers';
+
+function Home() {
+    return (
+        <div className="container mt-4">
+            {/*Latest Courses*/}
+            <h3 className="pb-1 mb-4">Latest courses <Link to="/all-courses" className="float-end">See All</Link></h3>
+            <div className="row">
+                <div className="col-md-3">
+                <div className="card">
+                    <Link to="/detail/1"><img src="logo512.png" className="card-img-top" alt="..."/></Link>
+                    <div className="card-body">
+                        <h5 className="card-title"><Link to="/detail/1">Course title</Link></h5>
+                        {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+                        {/* <a href="#" className="btn btn-primary">Details</a> */}
+                    </div>
+                    </div>
+                </div>
+                <div className="col-md-3">
+                <div className="card">
+                <Link to="/detail/2"><img src="logo512.png" className="card-img-top" alt="..."/></Link>
+                    <div className="card-body">
+                        <h5 className="card-title"><Link to="/detail/2">Course title</Link></h5>
+                        {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+                        {/* <a href="#" className="btn btn-primary">Details</a> */}
+                    </div>
+                    </div>
+                </div>
+                <div className="col-md-3">
+                <div className="card">
+                <Link to="/detail/3"><img src="logo512.png" className="card-img-top" alt="..."/></Link>
+                    <div className="card-body">
+                        <h5 className="card-title"><Link to="/detail/3">Course title</Link></h5>
+                        {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+                        {/* <a href="#" className="btn btn-primary">Details</a> */}
+                    </div>
+                    </div>
+                </div>
+                <div className="col-md-3">
+                <div className="card">
+                <Link to="/detail/4"><img src="logo512.png" className="card-img-top" alt="..."/></Link>
+                    <div className="card-body">
+                        <h5 className="card-title"><Link to="/detail/4">Course title</Link></h5>
+                        {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+                        {/* <a href="#" className="btn btn-primary">Details</a> */}
+                    </div>
+                    </div>
+                </div>
+            </div>
+            {/*End Latest Courses */}
+            {/*Popular Courses*/}
+            <h3 className="pb-1 mb-4 mt-5">Popular courses<Link to="/popular-courses" className="float-end">See All</Link></h3>
+            <div className="row mb-4">
+                <div className="col-md-3">
+                <div className="card">
+                    <a href="#"><img src="logo512.png" className="card-img-top" alt="..."/></a>
+                    <div className="card-body">
+                        <h5 className="card-title"><a href="#">Course title</a></h5>
+                        {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+                        {/* <a href="#" className="btn btn-primary">Details</a> */}
+                    </div>
+                    <div className="card-footer">
+                        <div className='title'> 
+                        <span>Rating: 4.5/5 </span>
+                        <span className='float-end'> Views: 78945 </span>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className="col-md-3">
+                <div className="card">
+                    <a href="#"><img src="logo512.png" className="card-img-top" alt="..."/></a>
+                    <div className="card-body">
+                        <h5 className="card-title"><a href="#">Course title</a></h5>
+                        {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+                        {/* <a href="#" className="btn btn-primary">Details</a> */}
+                    </div>
+                    <div className="card-footer">
+                        <div className='title'> 
+                        <span>Rating: 4.5/5 </span>
+                        <span className='float-end'> Views: 78945 </span>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className="col-md-3">
+                <div className="card">
+                    <a href="#"><img src="logo512.png" className="card-img-top" alt="..."/></a>
+                    <div className="card-body">
+                        <h5 className="card-title"><a href="#">Course title</a></h5>
+                        {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+                        {/* <a href="#" className="btn btn-primary">Details</a> */}
+                    </div>
+                    <div className="card-footer">
+                        <div className='title'> 
+                        <span>Rating: 4.5/5 </span>
+                        <span className='float-end'> Views: 78945 </span>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className="col-md-3">
+                <div className="card">
+                    <a href="#"><img src="logo512.png" className="card-img-top" alt="..."/></a>
+                    <div className="card-body">
+                        <h5 className="card-title"><a href="#">Course title</a></h5>
+                        {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+                        {/* <a href="#" className="btn btn-primary">Details</a> */}
+                    </div>
+                    <div className="card-footer">
+                        <div className='title'> 
+                        <span>Rating: 4.5/5 </span>
+                        <span className='float-end'> Views: 78945 </span>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            {/*End Popular Courses */}
+            {/*Popular Teachers*/}
+            <h3 className="pb-1 mb-4 mt-5">Popular Teachers<Link to="/popular-teachers" className="float-end">See All</Link></h3>
+            <div className="row mb-4">
+                <div className="col-md-3">
+                <div className="card">
+                    <Link to="/teacher-detail/1"><img src="logo512.png" className="card-img-top" alt="..."/></Link>
+                    <div className="card-body">
+                        <h5 className="card-title"><Link to="/teacher-detail/1">Teacher Name</Link></h5>
+                        {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+                        {/* <a href="#" className="btn btn-primary">Details</a> */}
+                    </div>
+                    <div className="card-footer">
+                        <div className='title'> 
+                        <span>Rating: 4.5/5 </span>                    
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className="col-md-3">
+                <div className="card">
+                    <Link to="/teacher-detail/1"><img src="logo512.png" className="card-img-top" alt="..."/></Link>
+                    <div className="card-body">
+                        <h5 className="card-title"><Link to="/teacher-detail/1">Teacher Name</Link></h5>
+                        {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+                        {/* <a href="#" className="btn btn-primary">Details</a> */}
+                    </div>
+                    <div className="card-footer">
+                        <div className='title'> 
+                        <span>Rating: 4.5/5 </span>                    
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className="col-md-3">
+                <div className="card">
+                    <Link to="/teacher-detail/1"><img src="logo512.png" className="card-img-top" alt="..."/></Link>
+                    <div className="card-body">
+                        <h5 className="card-title"><Link to="/teacher-detail/1">Teacher Name</Link></h5>
+                        {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+                        {/* <a href="#" className="btn btn-primary">Details</a> */}
+                    </div>
+                    <div className="card-footer">
+                        <div className='title'> 
+                        <span>Rating: 4.5/5 </span>                    
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className="col-md-3">
+                <div className="card">
+                    <Link to="/teacher-detail/1"><img src="logo512.png" className="card-img-top" alt="..."/></Link>
+                    <div className="card-body">
+                        <h5 className="card-title"><Link to="/teacher-detail/1">Teacher Name</Link></h5>
+                        {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+                        {/* <a href="#" className="btn btn-primary">Details</a> */}
+                    </div>
+                    <div className="card-footer">
+                        <div className='title'> 
+                        <span>Rating: 4.5/5 </span>                    
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            {/*End Popular Teacher */}
+            {/*Student Testimonial*/}
+            <h3 className="pb-1 mb-4 mt-5">Student Testimonial<a href="#" className="float-end">See All</a></h3>
+            <div id="carouselExampleIndicators" className="carousel slide bg-dark text-white py-5" data-bs-ride="carousel">
+                <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                    <figure className="text-center">
+                        <blockquote className="blockquote">
+                            <p>A well-known quote, contained in a blockquote element.</p>
+                        </blockquote>
+                        <figcaption className="blockquote-footer">
+                            Someone famous in <cite title="Source Title">Source Title</cite>
+                        </figcaption>
+                        </figure>
+                    </div>
+                    <div className="carousel-item">
+                    <figure className="text-center">
+                        <blockquote className="blockquote">
+                            <p>A well-known quote, contained in a blockquote element.</p>
+                        </blockquote>
+                        <figcaption className="blockquote-footer">
+                            Someone famous in <cite title="Source Title">Source Title</cite>
+                        </figcaption>
+                        </figure>
+                    </div>
+                    <div className="carousel-item">
+                    <figure className="text-center">
+                        <blockquote className="blockquote">
+                            <p>A well-known quote, contained in a blockquote element.</p>
+                        </blockquote>
+                        <figcaption className="blockquote-footer">
+                            Someone famous in <cite title="Source Title">Source Title</cite>
+                        </figcaption>
+                        </figure>
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+                </div>
+            {/*End Student Testimonial */}
+        </div>
+    );
+  }
+  
+  export default Home;
+  
---
+++ src/component/User/Login.js
@@ -0,0 +1,84 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const baseUrl = "http://127.0.0.1:8000/api";
+
+function Login() {
+    const [studenLoginData, setStudenLoginData] = useState({
+        'username': '',
+        'password': ''
+    });
+
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    const handleChange = (event) => {
+        setStudenLoginData({
+            ...studenLoginData,
+            [event.target.name]: event.target.value
+        });
+    }
+
+    const submitForm = async (event) => {
+        event.preventDefault();
+
+        const studentFormData = new FormData();
+        studentFormData.append("username", studenLoginData.username);
+        studentFormData.append("password", studenLoginData.password);
+
+        try {
+            const response = await axios.post(baseUrl + '/student-login', studentFormData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+
+            console.log('Server Response:', response.data);
+            if (response.data.bool === true) {
+                localStorage.setItem('studentLoginStatus', true);
+                window.location.href = '/user-dashboard';
+            }
+        } catch (error) {
+            console.log(error);
+            setErrorMsg("Login failed. Please check your credentials.");
+        }
+    };
+
+    const studentLoginStatus = localStorage.getItem('studentLoginStatus');
+    if (studentLoginStatus === 'true') {
+        window.location.href = '/user-dashboard';
+    }
+
+    useEffect(() => {
+        document.title = "Student Login";
+    }, []);
+
+    return (
+        <div className="container mt-4">
+            <div className="row">
+                <div className='col-6 offset-3'>
+                    <div className="card">
+                        <h5 className="card-header">User Login</h5>
+                        <div className="card-body">
+                            {errorMsg && <p className='text-danger'>{errorMsg}</p>}
+                            <form>
+                                <div className="m-3">
+                                    <label htmlFor="exampleInputEmail1" className="form-label">Username</label>
+                                    <input type="text" value={studenLoginData.username} name='username' onChange={handleChange} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                </div>
+                                <div className="m-3">
+                                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                                    <input type="password" value={studenLoginData.password} name='password' onChange={handleChange} className="form-control" id="exampleInputPassword1" />
+                                </div>
+                                <div className='m-3'>
+                                    <button type="submit" onClick={submitForm} className="btn btn-primary">Login</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
